6a675953bb028602b763e4b929cd430bd6b3a12c
Subash Jirel <subashjrl@gmail.com>
2024-09-28 18:33:45 +0545

    added functionalites for collisions

--- breakout.js
+++ breakout.js
@@ -17,6 +17,21 @@ let player = {
   velocityX: playerVelocityX,
 };
 
+//ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ballVelocityX = 3;
+let ballVelocityY = 2;
+
+let ball = {
+  x: boardWidth / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: ballVelocityX,
+  velocityY: ballVelocityY,
+};
+
 window.onload = function () {
   board = document.getElementById('board');
   board.height = boardHeight;
@@ -38,6 +53,30 @@ function update() {
   // player
   context.fillStyle = 'lightGreen';
   context.fillRect(player.x, player.y, player.width, player.height);
+
+  // ball
+  context.fillStyle = 'white';
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  //bounce the ball off player paddle
+  if (topCollision(ball, player) || bottomCollision(ball, player)) {
+    ball.velocityY *= -1; // flip y direction up or down
+  } else if (leftCollision(ball, player) || rightCollision(ball, player)) {
+    ball.velocityX *= -1; // flip x direction left or right
+  }
+
+  if (ball.y <= 0) {
+    // if ball touches top of canvas
+    ball.velocityY *= -1; //reverse direction
+  } else if (ball.x <= 0 || ball.x + ball.width >= boardWidth) {
+    // if ball touches left or right of canvas
+    ball.velocityX *= -1; //reverse direction
+  } else if (ball.y + ball.height >= boardHeight) {
+    // if ball touches bottom of canvas
+    //game over
+  }
 }
 
 function outOfBounds(xPosition) {
@@ -56,6 +95,34 @@ function movePlayer(e) {
     if (!outOfBounds(nextplayerX)) {
       player.x = nextplayerX;
     }
-    // player.x += player.velocityX;
   }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
+
+function topCollision(ball, block) {
+  //a is above b (ball is above block)
+  return detectCollision(ball, block) && ball.y + ball.height >= block.y;
+}
+
+function bottomCollision(ball, block) {
+  //a is above b (ball is below block)
+  return detectCollision(ball, block) && block.y + block.height >= ball.y;
+}
+
+function leftCollision(ball, block) {
+  //a is left of b (ball is left of block)
+  return detectCollision(ball, block) && ball.x + ball.width >= block.x;
+}
+
+function rightCollision(ball, block) {
+  //a is right of b (ball is right of block)
+  return detectCollision(ball, block) && block.x + block.width >= ball.x;
 }
