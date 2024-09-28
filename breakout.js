94999b2c259e3144c1b5cf63803342bffe137d06
Subash Jirel <subashjrl@gmail.com>
2024-09-28 16:42:20 +0545

    paddle can now move left and right

--- breakout.js
+++ breakout.js
@@ -7,12 +7,14 @@ let context;
 //players
 let playerWidth = 80; //500 for testing, 80 normal
 let playerHeight = 10;
+let playerVelocityX = 10; //move 10 pixels each time
 
 let player = {
   x: boardWidth / 2 - playerWidth / 2,
   y: boardHeight - playerHeight - 5,
   width: playerWidth,
   height: playerHeight,
+  velocityX: playerVelocityX,
 };
 
 window.onload = function () {
@@ -24,4 +26,36 @@ window.onload = function () {
   //draw initial player
   context.fillStyle = 'lightGreen';
   context.fillRect(player.x, player.y, player.width, player.height);
+
+  requestAnimationFrame(update);
+  document.addEventListener('keydown', movePlayer);
 };
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  // player
+  context.fillStyle = 'lightGreen';
+  context.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function outOfBounds(xPosition) {
+  return xPosition < 0 || xPosition + playerWidth > boardWidth;
+}
+
+function movePlayer(e) {
+  if (e.code == 'ArrowLeft') {
+    // player.x -= player.velocityX;
+    let nextplayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+  } else if (e.code == 'ArrowRight') {
+    let nextplayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+    // player.x += player.velocityX;
+  }
+}
